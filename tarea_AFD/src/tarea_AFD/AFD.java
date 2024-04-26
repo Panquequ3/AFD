5afbc19085c0578a95ef6c740c28141fc73da532
Panqueque <108034714+Panquequ3@users.noreply.github.com>
2024-04-26 17:41:48 -0400

    clase AFD actualizado

--- tarea_AFD/src/tarea_AFD/AFD.java
+++ tarea_AFD/src/tarea_AFD/AFD.java
@@ -1,6 +1,36 @@
 package tarea_AFD;
 
+import java.util.HashMap;
+
 public class AFD {
+	private char estado_ini; //q0
+	private HashMap<String,Estado> conjunto_estados; //Q
+	
+	//constructor
+	public AFD(String palabra_entrada, char estado_ini, HashMap<String, Estado> conjunto_estados) {
+		super();
+		this.estado_ini = estado_ini;
+		this.conjunto_estados = conjunto_estados;
+	}
+	
+
+	//getters (porsiacaso)
+	public char getEstado_ini() {
+		return estado_ini;
+	}
+
+
+	public HashMap<String, Estado> getConjunto_estados() {
+		return conjunto_estados;
+	}
+	
+	//funciones
+	public boolean lee_palabra() {
+		return true;
+	}
+
+
+
 	public static void main(String[] args) {
 		System.out.print("Bruja");
 	}
