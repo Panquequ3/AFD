283ce634582bef285bab404bbb6e5f52341a72ab
NigelMoon <pato200337@gmail.com>
2024-04-29 12:58:51 -0400

    Se hizo un merge con Estado y lo comente para entenderlo

--- tarea_AFD/src/tarea_AFD/Estado.java
+++ tarea_AFD/src/tarea_AFD/Estado.java
@@ -7,17 +7,24 @@ public class Estado {
 	private HashMap<String,String> Transiciones;
 	private boolean estado_final;
 	
+	//Constructor de estado
+	
 	public Estado(String value, boolean esfinal) {
 		this.value = value;
 		this.estado_final = esfinal;
 		this.Transiciones = new HashMap<String,String>();
 	}
 
+	//Funcion para agregar las transiciones a un estado, no permitiendo
+	//el ingreso repetido
+	
 	public void AgregarTransicion(String simbolo, String estado) {
 		if(!this.Transiciones.containsKey(simbolo)) {
 			this.Transiciones.put(simbolo, estado);
 		}
 	}
+	
+	//Funcion para devolver el estado al que va con un simbolo
 	
 	public String LeerSimbolo(String simbolo) {
 		if(this.Transiciones.containsKey(simbolo)) {
@@ -26,10 +33,14 @@ public class Estado {
 		return ""; //Tiene que analizarlo pa que no acepte llaves vacias
 	}
 	
+	//Estado para confirmar si un estado es final
+	
 	public boolean isFinal() {
 		return this.estado_final;
 	}
 
+	//Solo esta esta funcion porque me lo pide :p
+	
 	public String getValue() {
 		return value;
 	}
