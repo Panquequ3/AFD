c6732e29b09a5b0233f80361478b69df91b015b0
Panqueque <108034714+Panquequ3@users.noreply.github.com>
2024-04-30 16:36:03 -0400

    AFD actualizado

--- tarea_AFD/src/tarea_AFD/AFD.java
+++ tarea_AFD/src/tarea_AFD/AFD.java
@@ -49,8 +49,10 @@ public class AFD {
 	public static void main(String[] args) {
 		//en teoria el input debiese ser como:
 		String estado_inicial = "0";
-		String[] estados_finales = {"1"};
+		String est_finales = "1";
 		String transiciones = "(0,a,1)-(1,a,1)-(1,b,1)-(0,b,2)-(2,a,2)-(2,b,2)"; //desde el estado 0, con a, vamos al estado 1
+		String palabra = "aaaaaaaabbbbbaaabbbaabbabababababba"; //palabra a probar
+		
 		//pasando las transiciones a un arraylist
 		String[] tran = transiciones.split("-");
 		ArrayList<String> lista_transiciones = new ArrayList<>();
@@ -58,8 +60,10 @@ public class AFD {
 			elemento = elemento.replace("(", "").replace(")", "");
 			lista_transiciones.add(elemento);
 		}
+		//pasando estados_finales a un array
+		String[] estados_finales = est_finales.split(",");
 		
-		String palabra = "baaaaaaaabbbbbaaabbbaabbabababababba";
+		
 		
 		//llenando el AFD y creando los objetos Estado
 		
@@ -67,20 +71,43 @@ public class AFD {
 		HashMap<String,Estado> estados = new HashMap<>();
 		
 		//Creando objetos Estado y añadiendolos al conjunto
+		//1: añadir estados iniciales y finales inmediatamente
+		for(String simbolo : estados_finales) {
+			Estado aux = new Estado(simbolo, true);
+			estados.put(simbolo,aux);
+			for(String t : lista_transiciones) {
+				String[] datos = t.split(",");
+				if(simbolo.equals(datos[0])) {
+					aux.AgregarTransicion(datos[1],datos[2]);	
+					//datos[0]:estado 1, 2, etc
+					//datos[2]:a, b, etc
+				}		
+			}
+		}
+		
+		//si el estado inicial no es tambien estado final, entonces se agrega
+		if(estados.containsKey(estado_inicial)) {
+			Estado aux = new Estado(estado_inicial, false);
+			estados.put(estado_inicial,aux);
+			for(String t : lista_transiciones) {
+				String[] datos = t.split(",");
+				if(estado_inicial.equals(datos[0])) {
+					aux.AgregarTransicion(datos[1],datos[2]);	
+				}
+					
+			}
+		}
+		//2: añadir los estados restantes a partir de las transiciones:
 		for(String transicion : lista_transiciones) {
 			String[] partes = transicion.split(",");
 			String simbolo = partes[0];
 			//si es nulo, entonces no está el objeto, por ello lo crea y lo agrega
 			if(estados.get(simbolo) == null) {
-				int indice = Arrays.binarySearch(estados_finales, simbolo + "");
-				boolean es_final = (indice >= 0) ? true : false; //indica si el estado simbolo es e. final o no
-				Estado aux = new Estado(simbolo, es_final);
+				Estado aux = new Estado(simbolo, false);
 				for(String t : lista_transiciones) {
 					String[] datos = t.split(",");
 					if(simbolo.equals(datos[0])) {
-						String sim = datos[2]; //estado 1, 2, etc
-						String key = datos[1]; //a, b, etc
-						aux.AgregarTransicion(key, sim);	
+						aux.AgregarTransicion(datos[1],datos[2]);	
 					}
 						
 				}
